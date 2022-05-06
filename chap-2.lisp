47480fa534cc96a47fdd1dfed7aa47abe66d4c82
Jason Robinson <json.robinson@gmail.com>
2022-05-06 13:59:12 -0500

    Completed exercise 2.1

---
+++ chap-2.lisp
@@ -0,0 +1,73 @@
+;; Solutions for Chapter 2 exercises.
+;; 
+;; This file is meant to be loaded or compiled \(or both).
+;; As packages were not introduced, I decided to keep it simple and stand-alone.
+;;
+;; Tests are at the bottom of this file, or they can be run anytime with
+;;
+;; > (c2-examples)
+;; > (c2-props)
+;; > (progn (c2-examples) (c2-props))
+;;
+
+(in-package :cl-user)
+
+;; PAIP DEFINITIONS -- Code from the book required for the exercises
+
+(defparameter *simple-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Noun))
+    (verb-phrase -> (Verb noun-phrase))
+    (Article -> the a)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked))
+  "A grammar for a trivial subset of English.")
+
+(defvar *grammar* *simple-grammar*
+  "The grammar used by generate.  Initially, this is
+  *simple-grammar*, but we can switch to other grammars.")
+
+(defun one-of (set)
+  "Pick one element of set, and make a list of it."
+  (list (random-elt set)))
+
+(defun random-elt (choices)
+  "Choose an element from a list at random."
+  (elt choices (random (length choices))))
+
+(defun rule-lhs (rule)
+  "The left-hand side of a rule."
+  (first rule))
+
+(defun rule-rhs (rule)
+  "The right-hand side of a rule."
+  (rest (rest rule)))
+
+(defun rewrites (category)
+  "Return a list of the possible rewrites for this category."
+  (rule-rhs (assoc category *grammar*)))
+
+;; Had to rename so it doesn't clash with CHECK-IT:GENERATE
+(defun generate-phrase (phrase)
+  "Generate a random sentence or phrase"
+  (cond ((listp phrase)
+         (mappend #'generate-phrase phrase))
+        ((rewrites phrase)
+         (generate-phrase (random-elt (rewrites phrase))))
+        (t (list phrase))))
+
+;; EXERCISES -- 2.1
+
+(defun generate-english (phrase)
+  "Generates a random english sentence or phrase.
+> (generate-english 'sentence) => (A MAN SAW THE MAN)
+> (generate-english 'sentence) => (THE MAN TOOK A BALL)
+> (generate-english 'noun-phrase) => (THE WOMAN)
+> (generate-english 'verb-phrase) => (HIT A WOMAN)
+> (generate-english 'word) => (WORD)"
+  (let ((choices (rewrites phrase)))
+    (cond ((listp phrase)
+	   (mappend #'generate-english phrase))
+	  ((null choices)
+	   (list phrase))
+	  (t  (generate-english (random-elt choices))))))
