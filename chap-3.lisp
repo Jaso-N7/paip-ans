856e9bdeb904397076dab99849af58edc37b58bc
Jason Robinson <json.robinson@gmail.com>
2022-05-08 11:39:39 -0500

    Testing example for 3.3

--- chap-3.lisp
+++ chap-3.lisp
@@ -6,6 +6,24 @@
     (* x x)))
  6)
 
+;; 3.2 LIST* ?
+
+;; 3.3
+
+((lambda (lis)
+   (labels
+       ((rec (l res)
+	  (cond ((endp res)
+		 (princ l))
+		((null (cdr res))
+		 (princ l)
+		 (princ ".")
+		 (rec (first res) (cdr res)))
+		(t (princ l)
+		   (rec (first res) (cdr res))))))
+     (rec (first lis) (rest lis))))
+ '(a b c d c e c))
+
 ;;; TESTS
 
 (unless (find-package :ptester)
